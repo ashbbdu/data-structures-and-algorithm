187decd54b161767d5ab88ba425fe08bffb79430
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2025-01-19 21:54:37 +0530

    Comparator

--- src/Basics/CusotomComaparatorTut.java
+++ src/Basics/CusotomComaparatorTut.java
@@ -1,4 +1,26 @@
 package Basics;
 
+import java.util.*;
+
 public class CusotomComaparatorTut {
+    public static void main(String[] args) {
+        List <Integer> al = new ArrayList<>();
+        al.add(4);
+        al.add(3);
+        al.add(1);
+        al.add(2);
+        Collections.sort(al, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer num1, Integer num2) {
+                if(num1 < num2) {
+                    return 1;
+                } else if (num1 > num2) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        System.out.println(al);
+    }
 }
